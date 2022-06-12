b6e029104a444355719a0c632416d9c5cd0665e7
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-12 15:34:30 +0000

    Storage

---
+++ lib/Services/Storagemethods.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+class StorageMethods{
+  final FirebaseStorage _Storage= FirebaseStorage.instance;
+}
--- lib/Services/auth.dart
+++ lib/Services/auth.dart
@@ -121,7 +121,9 @@ class AuthService{
   Future CurrentUser()async{
     try {
       User? currentu = await _auth.currentUser;
-      await _firestore.collection("users").doc(currentu!.uid).get();
+      await _firestore.collection("users").doc(currentu!.uid).get(
+        
+      );
       return fbuser(currentu);
     }catch(e){
       print(e.toString());
