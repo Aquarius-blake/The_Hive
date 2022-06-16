6a34130f50d82a5ca737416cd089f2b9fab48db1
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-16 18:35:56 +0000

    Loading Screen NB:Still on break though

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class Loading extends StatelessWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlueAccent,
+      child: Center(
+
+      ),
+    );
+  }
+}
