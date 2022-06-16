28ea5b435978f8fa1c64583cd310c12027a9a2e5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-16 18:58:22 +0000

    Loading Screen NB:Still on break though

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatelessWidget {
@@ -9,7 +10,10 @@ class Loading extends StatelessWidget {
     return Container(
       color: Colors.lightBlueAccent,
       child: Center(
-
+        child: SpinKitHourGlass(
+            color: Colors.white,
+                size: 50.0,
+        ),
       ),
     );
   }
