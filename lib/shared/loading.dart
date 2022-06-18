818be8363ce8f4493305e20c7b3efa09c3481e7f
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-18 16:24:45 +0000

    Loading Screen. NB:Still on break though google fonts

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -11,25 +11,28 @@ class Loading extends StatelessWidget {
     return Container(
       color: Colors.lightBlueAccent,
       child: Center(
-        child: Column(
-          children: [
-            SpinKitHourGlass(
-                color: Colors.white,
-                    size: 100.0,
-            ),
-            SizedBox(height: 5.0,),
-            Text(
-                "Loading, Please Wait A Moment",
-              style: GoogleFonts.fascinate(
-                textStyle: TextStyle(
+        child: Container(
+          child: Column(
+            children: [
+              SpinKitHourGlass(
+                  color: Colors.white,
+                      size: 100.0,
 
-                ),
-                fontStyle: FontStyle.italic,
-                fontSize: 25.0,
+              ),
+              SizedBox(height: 5.0,),
+              Text(
+                  "Loading, Please Wait A Moment",
+                style: GoogleFonts.fascinate(
+                  textStyle: TextStyle(
+                            color: Colors.white,
+                  ),
+                  fontStyle: FontStyle.italic,
+                  fontSize: 25.0,
 
+                )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
