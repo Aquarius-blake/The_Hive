1e26d4b709c2efbdfa1622533690fc4cca377df5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-20 16:26:31 +0000

    Seperating App views

--- lib/Screens/Platforms/Layout.dart
+++ lib/Screens/Platforms/Layout.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:forum3/Screens/Home/home.dart';
+import 'package:forum3/Screens/Platforms/Web.dart';
+import 'package:forum3/shared/dim.dart';
 
 class Layout extends StatelessWidget {
+
   const Layout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          if(constraints.maxWidth >websreensize){
+            return Webview();
+          }else{
+            return Home();
+          }
+        }
+        );
+
   }
 }
