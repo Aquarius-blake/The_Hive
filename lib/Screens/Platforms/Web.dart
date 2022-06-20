51b6a42424e2b0fc6f0eef0098c165c732cf190a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-20 16:15:04 +0000

    Seperating App views

---
+++ lib/Screens/Platforms/Web.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class Webview extends StatefulWidget {
+  const Webview({Key? key}) : super(key: key);
+
+  @override
+  State<Webview> createState() => _WebviewState();
+}
+
+class _WebviewState extends State<Webview> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          
+        )
+    );
+  }
+}
