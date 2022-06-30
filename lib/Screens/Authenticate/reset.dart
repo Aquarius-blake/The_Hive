f34cc311567f1a28ab11fae5a18dff578160c658
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-30 20:09:33 +0000

    Reset password

--- lib/Screens/Authenticate/reset.dart
+++ lib/Screens/Authenticate/reset.dart
@@ -77,7 +77,7 @@ fontSize: 30.0,
                         ElevatedButton(
                           onPressed: () async{
                             if(_formKey.currentState?.validate()!=null){
-                              print(email);
+
                               await _auth.Reset(email);
                               Navigator.pop(context);
                             }
