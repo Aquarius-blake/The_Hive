81f27d9ec9dbfcadfd962a588b55ef85176a6463
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-06 23:14:45 +0000

    user

--- lib/Services/Storagemethods.dart
+++ lib/Services/Storagemethods.dart
@@ -1,10 +1,10 @@
 
 //Taking a moderately long break
-
 /*
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
 
 class StorageMethods{
@@ -12,12 +12,12 @@ class StorageMethods{
   final FirebaseAuth _auth=FirebaseAuth.instance;
 
 
-
+//add image to storage
   Future <String?> Storageip(String name,dynamic file,bool isPost) async{
-    Reference ref=_Storage.ref().child(name).child(_auth.currentUser!.uid)
+    Reference ref=_Storage.ref().child(name).child(_auth.currentUser!.uid);
     UploadTask uploadTask= ref.putFile(file);
     TaskSnapshot snap= await uploadTask;
-    snap.ref.getDownloadURL() ;
+    String downloadurl= snap.ref.getDownloadURL().toString() ;
 
     return ;
   }
