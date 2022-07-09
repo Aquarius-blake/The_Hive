dc2ec668315ee55313627f0ae0a2098c4592ca2b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-09 20:30:26 +0000

    Redesign

--- lib/Screens/Authenticate/reset.dart
+++ lib/Screens/Authenticate/reset.dart
@@ -23,98 +23,104 @@ bool e=false;
         title: Text("Reset Password"),
       ),
       body: SafeArea(
-        child: Card(
-          child: Container(
+        
+        child: Container(
+          padding: const EdgeInsets.all(200),
+          child: Card(
+            margin: EdgeInsets.fromLTRB(16.0, 16.0,16.0, 0),
+            color: Colors.white,
+            child: Container(
 
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Password Reset",
-                          style: TextStyle(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Password Reset",
+                            style: TextStyle(
 fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white
-                          ),),
-                          Container(
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(100, 10, 100, 30),
-                              child: TextFormField(
-                                validator: (val)=>val!.isEmpty ? "Please Enter A Valid Email" : null,
-                                onChanged: (val){
-                                  setState(() {
-                                    email=val;
-                                  });
-                                },
-                                decoration: InputDecoration(
-                                  hintText: "Enter Email Address",
-                                  filled: true,
-                                  fillColor: Colors.white70,
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(100.0),
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                            ),),
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(100, 10, 100, 30),
+                                child: TextFormField(
+                                  validator: (val)=>val!.isEmpty ? "Please Enter A Valid Email" : null,
+                                  onChanged: (val){
+                                    setState(() {
+                                      email=val;
+                                    });
+                                  },
+                                  decoration: InputDecoration(
+                                    hintText: "Enter Email Address",
+                                    filled: true,
+                                    fillColor: Colors.white70,
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(100.0),
+
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.redAccent),
+                                      borderRadius: BorderRadius.circular(100.0),
+                                    ),
+                                  ),
+
+                                  style: TextStyle(
 
                                   ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.redAccent),
-                                    borderRadius: BorderRadius.circular(100.0),
-                                  ),
-                                ),
-
-                                style: TextStyle(
-
                                 ),
                               ),
                             ),
-                          ),
-                          SizedBox(height: 20,),
-                          ElevatedButton(
-                            onPressed: () async{
-                              if(_formKey.currentState?.validate()!=null){
+                            SizedBox(height: 20,),
+                            ElevatedButton(
+                              onPressed: () async{
+                                if(_formKey.currentState?.validate()!=null){
 
-                                await _auth.Reset(email);
-                                Navigator.pop(context);
-                              }
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(15.0),
-                              child: Text(
-                                  "Reset Password",
-                                      style: TextStyle(
-                                        color: Colors.blue
-                                      ),
-                              ),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                                elevation: 6.0,
-                                shadowColor: Colors.black,
-                                primary: Colors.white,
-                                side: BorderSide(
-                                  color: Colors.blue,
-                                  width: 2.0,
+                                  await _auth.Reset(email);
+                                  Navigator.pop(context);
+                                }
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Text(
+                                    "Reset Password",
+                                        style: TextStyle(
+                                          color: Colors.blue
+                                        ),
                                 ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  elevation: 6.0,
+                                  shadowColor: Colors.black,
+                                  primary: Colors.white,
+                                  side: BorderSide(
+                                    color: Colors.blue,
+                                    width: 2.0,
+                                  ),
 
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(100.0)
-                                )
-                            ),
-                          )
-                        ],
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(100.0)
+                                  )
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
 
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
 
+            ),
           ),
         ),
       ),
