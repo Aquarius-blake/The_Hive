8bfbfe250bc87e989549360ab88497c881cf9300
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-18 19:23:21 +0000

    pages

---
+++ lib/Screens/Home/Mobilepages/Msearch.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Msearch extends StatefulWidget {
+  const Msearch({Key? key}) : super(key: key);
+
+  @override
+  State<Msearch> createState() => _MsearchState();
+}
+
+class _MsearchState extends State<Msearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
