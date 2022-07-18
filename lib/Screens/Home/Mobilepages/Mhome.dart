033ce1491dd3ad6e7c504322331b3595aa287f95
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-18 19:22:23 +0000

    pages

--- lib/Screens/Home/Mobilepages/Mhome.dart
+++ lib/Screens/Home/Mobilepages/Mhome.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+
+class Mhome extends StatefulWidget {
+  const Mhome({Key? key}) : super(key: key);
+
+  @override
+  State<Mhome> createState() => _MhomeState();
+}
+
+class _MhomeState extends State<Mhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("homepage"),
+    );
+  }
+}
