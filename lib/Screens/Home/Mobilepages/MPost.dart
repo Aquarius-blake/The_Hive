615902e703c5c6acfaaa1b43f6c250d3b1a9db04
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-21 21:12:02 +0000

    Mobile pages

--- lib/Screens/Home/Mobilepages/MPost.dart
+++ lib/Screens/Home/Mobilepages/MPost.dart
@@ -64,7 +64,8 @@ class _MpostState extends State<Mpost> {
                             border: InputBorder.none,
                           ),
                         ),
-                        Divider()
+                        Divider(),
+                        Row()
                       ],
                     ),
                   ),
