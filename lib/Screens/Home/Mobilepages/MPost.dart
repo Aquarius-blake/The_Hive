4cb04e61c645eaacc356f68a628678a5ab8bfa1e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-21 17:32:56 +0000

    Web pages

---
+++ lib/Screens/Home/Mobilepages/MPost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mpost extends StatefulWidget {
+  const Mpost({Key? key}) : super(key: key);
+
+  @override
+  State<Mpost> createState() => _MpostState();
+}
+
+class _MpostState extends State<Mpost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
