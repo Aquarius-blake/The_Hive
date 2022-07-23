70dca164248a564f41823a80af7edbe879c6fe82
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-23 20:15:17 +0000

    Mobile pages

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -9,6 +9,7 @@ import 'package:flutter/widgets.dart';
 import 'package:forum3/Models/Users1.dart';
 import 'package:forum3/Screens/Home/Mobilepages/MPost.dart';
 import 'package:forum3/Screens/Home/Mobilepages/Mhome.dart';
+import 'package:forum3/Screens/Home/Mobilepages/Mnotifications.dart';
 import 'package:forum3/Screens/Home/Mobilepages/Msearch.dart';
 import 'package:forum3/Services/Storagemethods.dart';
 import 'package:forum3/Services/Upload.dart';
@@ -305,7 +306,7 @@ class _HomeState extends State<Home> {
           Mhome(),
           Msearch(),
           Mpost(),
-          Text("Notification"),
+          Notifications(),
           Text("Messages")
 
         ],
