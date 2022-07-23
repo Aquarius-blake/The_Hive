de09321cab833a8e8732abe8656e4bcce448189d
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-23 00:10:14 +0000

    Mobile pages

---
+++ lib/Screens/Home/Mobilepages/Mnotifications.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Notifications extends StatefulWidget {
+  const Notifications({Key? key}) : super(key: key);
+
+  @override
+  State<Notifications> createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
