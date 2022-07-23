3f1864abfbfc4220fbc114f910d1ca9faa7289c0
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-23 21:12:55 +0000

    Mobile pages

--- lib/shared/Pop_up.dart
+++ lib/shared/Pop_up.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+
+Showsnackbar(String content,BuildContext context){
+  ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          content: Text(content)
+      )
+  );
+}
