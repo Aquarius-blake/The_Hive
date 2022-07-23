c1c684bdb51f6f2fc0c888ac08e0f6d6238a2d0c
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-23 20:16:21 +0000

    Mobile pages

---
+++ lib/Screens/Home/Mobilepages/Mmessages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mmessages extends StatefulWidget {
+  const Mmessages({Key? key}) : super(key: key);
+
+  @override
+  State<Mmessages> createState() => _MmessagesState();
+}
+
+class _MmessagesState extends State<Mmessages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
