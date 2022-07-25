6384f0ac0e9739b76788f2386ef743169db2a20e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-25 18:33:07 +0000

    Minor changes

--- lib/Services/Firestoremethods.dart
+++ lib/Services/Firestoremethods.dart
@@ -3,4 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreMethods{
     final FirebaseFirestore _firestore=FirebaseFirestore.instance;
+
+    //Upload post
+Future <String> Uploadpost()async{
+    return await "";
+}
 }
