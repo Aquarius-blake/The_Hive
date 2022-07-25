4bc4298ef9e2faccd8d839d2470350337875761d
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-25 03:55:01 +0000

    error handling

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -10,6 +10,7 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
       body: SafeArea(
         child: Center(
           child: Container(
