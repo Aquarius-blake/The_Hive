ffc9a6b28a5c500a852efea88abca4d930fedd13
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-25 22:25:59 +0000

    Posting

--- lib/Screens/Home/Mobilepages/MPost.dart
+++ lib/Screens/Home/Mobilepages/MPost.dart
@@ -157,7 +157,7 @@ class _MpostState extends State<Mpost> {
                         SingleChildScrollView(
                           child: TextField(
                             controller: _textEditingController2,
-                            maxLines: 9,
+                            maxLines: 6,
                             decoration: const InputDecoration(
                               hintText: "Write Something.....",
                               border: InputBorder.none,
