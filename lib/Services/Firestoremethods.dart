5393a017b853820ec050fc3cf781404b477e69c5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-25 18:46:56 +0000

    Minor changes

--- lib/Services/Firestoremethods.dart
+++ lib/Services/Firestoremethods.dart
@@ -1,11 +1,32 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:forum3/Services/Storagemethods.dart';
+import 'package:forum3/shared/Pop_up.dart';
 
 class FirestoreMethods{
     final FirebaseFirestore _firestore=FirebaseFirestore.instance;
 
     //Upload post
-Future <String> Uploadpost()async{
-    return await "";
+Future <String> Uploadpost(
+    BuildContext context,
+    String Title,
+    String Details,
+    dynamic file,
+    String uid,
+    )async{
+    String photourl;
+    try{
+        if(file!=null){
+             photourl=await StorageMethods().Storageip("Posts", file, true);
+        }
+        else{
+            photourl="";
+        }
+    }
+    catch(e){
+     String  err=e.toString();
+     Showsnackbar(err,context);
+    }
 }
 }
