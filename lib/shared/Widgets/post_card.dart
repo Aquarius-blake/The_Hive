55d003fb00eda7ea60284d1bb7cb7f8c1846b5d5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-26 00:10:45 +0000

    Posting

---
+++ lib/shared/Widgets/post_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
