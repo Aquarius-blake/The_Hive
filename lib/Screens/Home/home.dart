548153b9aba54b85a52bf30f8238b23c9de9cbd7
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-27 22:20:17 +0000

    Post card and feed

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -129,7 +129,7 @@ class _HomeState extends State<Home> {
         elevation: 2.0,
         title: Container(
           padding: EdgeInsets.only(
-            right: 30
+            right: 40
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
