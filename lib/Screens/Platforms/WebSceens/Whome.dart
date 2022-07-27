e2d242712c6166d4b18b633352d2f794488c4f33
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-27 00:04:30 +0000

    Post card and feed

--- lib/Screens/Platforms/WebSceens/Whome.dart
+++ lib/Screens/Platforms/WebSceens/Whome.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class WebHome extends StatefulWidget {
+  const WebHome({Key? key}) : super(key: key);
+
+  @override
+  State<WebHome> createState() => _WebHomeState();
+}
+
+class _WebHomeState extends State<WebHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
