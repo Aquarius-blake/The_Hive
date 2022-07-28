1b5c294216881d68111ebd327423892ac46124df
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-28 22:44:14 +0000

    Post card and feed functionality

--- lib/shared/Widgets/post_card.dart
+++ lib/shared/Widgets/post_card.dart
@@ -32,152 +32,155 @@ class _PostCardState extends State<PostCard> {
         bottom: 5,
       ),
       child: Card(
-        child: Column(
-          children: [
-            Row(
-              children: [
-
-                CircleAvatar(
-                  radius: 16,
-                  backgroundImage: NetworkImage(widget.snap['Profile Pic']),
-                ),
-                Expanded(
-                    child: Padding(
-                        child:Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.snap['author'],
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold
-                              ),)
-                          ],
-                        ) ,
-                        padding:const EdgeInsets.only(
-                            left: 10.0
-                        )
-                    )
-                ),
-                IconButton(
-                  onPressed: (){
-                    showDialog(context: context, builder: (context)=>Dialog(
-                      child: ListView(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 16,),
-                        shrinkWrap: true,
-                        children: [
-                          'Edit',
-                          'Delete'
-                        ].map((e) => InkWell(
-                          onTap: (){},
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                              vertical: 12,
-                              horizontal: 16,
-                            ),
-                            child: Text(e),
-                          ),
-                        )
-                        ).toList(),
-                      ),
-                    )
-                    );
-                  },
-                  icon: const Icon(
-                    Icons.more_vert,
-                    color: Colors.black,
-                  ),
-                )
-              ],
-            ),
-            Container(
-              padding:const EdgeInsets.only(
-                top: 5,
-              ),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width*0.8,
-                child: Text(
-                  widget.snap['title'],
-                  style: const TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding:const EdgeInsets.only(
-                top: 5,
-              ),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width*0.8,
-                child: Text(
-                  widget.snap['detail'],
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ),
-            Postimage(widget.snap['Image Url'], context),
-            Container(
-              padding: EdgeInsets.only(
-                top: 3.0,
-              ),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child:   Text(
-                  "Date",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12,
-
-                  ),
-
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 30,
-              ),
-              child: Row(
+        child: Container(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  Text(
-                    "123",
-                  ),
-                  IconButton(
-                      onPressed: (){},
-                      icon: Icon(
-                        Icons.favorite_border_outlined,
-                        color: Colors.red,
-                      )
+
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundImage: NetworkImage(widget.snap['Profile Pic']),
                   ),
                   Expanded(
-                      child: SizedBox()
-                  ),
-                  Text("12344"),
-                  IconButton(
-                      onPressed: (){},
-                      icon: Icon(
-                        Icons.comment_outlined,
-                        color: Colors.black,
+                      child: Padding(
+                          child:Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.snap['author'],
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold
+                                ),)
+                            ],
+                          ) ,
+                          padding:const EdgeInsets.only(
+                              left: 10.0
+                          )
                       )
                   ),
-                  Expanded(
-                      child: SizedBox()
-                  ),
                   IconButton(
-                      onPressed: (){},
-                      icon: Icon(
-                        Icons.share,
-                        color: Colors.black,
+                    onPressed: (){
+                      showDialog(context: context, builder: (context)=>Dialog(
+                        child: ListView(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,),
+                          shrinkWrap: true,
+                          children: [
+                            'Edit',
+                            'Delete'
+                          ].map((e) => InkWell(
+                            onTap: (){},
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                vertical: 12,
+                                horizontal: 16,
+                              ),
+                              child: Text(e),
+                            ),
+                          )
+                          ).toList(),
+                        ),
                       )
-                  ),
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.more_vert,
+                      color: Colors.black,
+                    ),
+                  )
                 ],
               ),
-            )
-          ],
+              Container(
+                padding:const EdgeInsets.only(
+                  top: 5,
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: Text(
+                    widget.snap['title'],
+                    style: const TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding:const EdgeInsets.only(
+                  top: 5,
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: Text(
+                    widget.snap['detail'],
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Postimage(widget.snap['Image Url'], context),
+              Container(
+                padding: EdgeInsets.only(
+                  top: 3.0,
+                ),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child:   Text(
+                    "Date",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
+
+                    ),
+
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "123",
+                    ),
+                    IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.favorite_border_outlined,
+                          color: Colors.red,
+                        )
+                    ),
+                    Expanded(
+                        child: SizedBox()
+                    ),
+                    Text("12344"),
+                    IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.comment_outlined,
+                          color: Colors.black,
+                        )
+                    ),
+                    Expanded(
+                        child: SizedBox()
+                    ),
+                    IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.share,
+                          color: Colors.black,
+                        )
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
