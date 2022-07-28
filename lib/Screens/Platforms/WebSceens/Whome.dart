4d345ac821d5f4ddf310fcb593d96522cc3e33fb
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-28 23:13:51 +0000

    Post card and feed functionality

--- lib/Screens/Platforms/WebSceens/Whome.dart
+++ lib/Screens/Platforms/WebSceens/Whome.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:forum3/shared/Widgets/post_card.dart';
 
@@ -15,7 +16,24 @@ class _WebHomeState extends State<WebHome> {
     return Container(
       child: SizedBox(
         width: MediaQuery.of(context).size.width*0.9,
-          child: PostCard()
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('Posts').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
+              if(snapshot.connectionState==ConnectionState.waiting){
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) => Container(
+                    child: PostCard(
+                      snap: snapshot.data!.docs[index].data(),
+                    ),
+                  )
+              );
+            },
+          ),
       ),
     );
   }
