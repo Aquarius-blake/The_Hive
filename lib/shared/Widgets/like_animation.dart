b69d71894d0ca3c28f852325f33fcb008b0fd78f
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-28 23:23:13 +0000

    Post card and feed functionality + like animation

---
+++ lib/shared/Widgets/like_animation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class likeAnimation extends StatefulWidget {
+  const likeAnimation({Key? key}) : super(key: key);
+
+  @override
+  State<likeAnimation> createState() => _likeAnimationState();
+}
+
+class _likeAnimationState extends State<likeAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
