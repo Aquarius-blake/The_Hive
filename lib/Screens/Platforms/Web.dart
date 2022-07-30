8bae916e6c544f8cb9b2c21dbccdca5e081bacd1
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-30 17:41:42 +0000

    Web ver. redesign

--- lib/Screens/Platforms/Web.dart
+++ lib/Screens/Platforms/Web.dart
@@ -232,7 +232,7 @@ class _WebviewState extends State<Webview> {
                                     ),
                                   ),
                                   SizedBox(
-                                    width: MediaQuery.of(context).size.width*0.05,
+                                    width: MediaQuery.of(context).size.width*0.1,
                                     height: MediaQuery.of(context).size.height*0.05,
                                   )
                                 ],
