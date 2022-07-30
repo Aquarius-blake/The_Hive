fda37be4a2361e83052bb12c69c62c8367a29434
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-30 16:21:54 +0000

    Web ver. redesign

--- lib/Screens/Platforms/WebSceens/wpost.dart
+++ lib/Screens/Platforms/WebSceens/wpost.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../Services/Firestoremethods.dart';
 import '../../../Services/Upload.dart';
+import '../../../shared/Pop_up.dart';
+import '../../../shared/error_handling.dart';
 
 class wpost extends StatefulWidget {
   const wpost({Key? key}) : super(key: key);
@@ -15,6 +18,30 @@ class _wpostState extends State<wpost> {
   final TextEditingController _textEditingController=TextEditingController();
   final TextEditingController _textEditingController2=TextEditingController();
   Upload Selection=Upload();
+
+  void _posting(String uid,String author,dynamic profilepic)async{
+    setState(() {
+      _isloading=true;
+    });
+    try{
+      String res=await FirestoreMethods().Uploadpost(_textEditingController.text, _textEditingController2.text, _image, uid, author, profilepic);
+      if(res=="success"){
+        setState(() {
+          _isloading=false;
+        });
+        Showsnackbar("Post Successful", context);
+      }
+      else{
+        setState(() {
+          _isloading=false;
+        });
+        Showsnackbar(res, context);
+      }
+    }catch(e){
+      String err=e.toString();
+      errormessage(err, context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
