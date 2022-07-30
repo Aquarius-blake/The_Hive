28f3665b7c3aacf8cc623329049bbbb334bb80af
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-30 22:33:13 +0000

    Like Animation

---
+++ lib/Screens/Home/Mobilepages/Mcomments_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class McommentsScreen extends StatefulWidget {
+  const McommentsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<McommentsScreen> createState() => _McommentsScreenState();
+}
+
+class _McommentsScreenState extends State<McommentsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
