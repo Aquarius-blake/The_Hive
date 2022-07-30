be21d72e1a485e04ac1618517cbf8f051f7beb44
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-30 16:19:17 +0000

    Web ver. redesign

---
+++ lib/Screens/Platforms/WebSceens/wpost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class wpost extends StatefulWidget {
+  const wpost({Key? key}) : super(key: key);
+
+  @override
+  State<wpost> createState() => _wpostState();
+}
+
+class _wpostState extends State<wpost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
