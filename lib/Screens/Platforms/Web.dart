5df1d96df07245d36f62f367e3c457fe91fc4e0c
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-30 16:41:08 +0000

    Web ver. redesign

--- lib/Screens/Platforms/Web.dart
+++ lib/Screens/Platforms/Web.dart
@@ -266,7 +266,7 @@ class _WebviewState extends State<Webview> {
                                       Navitap(page);
                                     },
                                     icon: FaIcon(FontAwesomeIcons.featherPointed),
-                                    iconSize: 20.0,
+                                    iconSize: 25.0,
 
                                   ),
 
