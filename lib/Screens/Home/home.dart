9aa58bcda9f7bbddcf73f2cedb280ec9df863cbe
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-31 00:06:52 +0000

    Like Animation

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -148,7 +148,7 @@ class _HomeState extends State<Home> {
       ),
 
       drawer: Drawer(
-        backgroundColor: Colors.white70,
+        backgroundColor: Colors.white,
         child: Container(
           child: SingleChildScrollView(
             child: Column(
