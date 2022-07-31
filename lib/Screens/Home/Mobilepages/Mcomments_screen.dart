d49890ff174ccd75a5d9e8b64858097ee3b53407
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-31 21:24:02 +0000

    Comments Section

--- lib/Screens/Home/Mobilepages/Mcomments_screen.dart
+++ lib/Screens/Home/Mobilepages/Mcomments_screen.dart
@@ -55,7 +55,7 @@ class _McommentsScreenState extends State<McommentsScreen> {
                   ),
                 ),
                 InkWell(
-
+onTap: (){},
                 )
               ],
             ),
