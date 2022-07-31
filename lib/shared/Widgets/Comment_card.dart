1a2781a3169b82c5ee1223d60703422a92bc76fc
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-31 21:41:08 +0000

    Comments Section

---
+++ lib/shared/Widgets/Comment_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Commentcard extends StatefulWidget {
+  const Commentcard({Key? key}) : super(key: key);
+
+  @override
+  State<Commentcard> createState() => _CommentcardState();
+}
+
+class _CommentcardState extends State<Commentcard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
