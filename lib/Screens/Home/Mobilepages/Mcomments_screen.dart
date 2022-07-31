7d80e8d7f3f8950fc9e714518c8e77411239bccb
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-07-31 21:02:59 +0000

    Comments Section

--- lib/Screens/Home/Mobilepages/Mcomments_screen.dart
+++ lib/Screens/Home/Mobilepages/Mcomments_screen.dart
@@ -11,19 +11,19 @@ class _McommentsScreenState extends State<McommentsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-backgroundColor: Colors.white,
-  title:const Text("Comments"),
-  centerTitle: true,
-),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title:const Text("Comments"),
+        centerTitle: true,
+      ),
       bottomNavigationBar: SafeArea(
           child: Container(
-height: kToolbarHeight,
+            height: kToolbarHeight,
             margin: EdgeInsets.only(
-bottom: MediaQuery.of(context).viewInsets.bottom
+                bottom: MediaQuery.of(context).viewInsets.bottom
             ),
             padding: const EdgeInsets.only(
-left: 16,
+              left: 16,
               right: 8,
             ),
           )
