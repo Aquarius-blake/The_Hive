c89a5c99c6ee8264ee592c29528575409f2e30bd
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-01 23:31:01 +0000

    Comments Section

--- lib/Services/Firestoremethods.dart
+++ lib/Services/Firestoremethods.dart
@@ -9,78 +9,87 @@ class FirestoreMethods{
     final FirebaseFirestore _firestore=FirebaseFirestore.instance;
 
     //Upload post
-Future <String> Uploadpost(
-    String Title,
-    String Details,
-    dynamic file,
-    String uid,
-    String author,
-    String ppurl
-    )async{
-    String photourl;
-    String err;
-    try{
-        if(file!=null){
-             photourl=await StorageMethods().Storageip("Posts", file, true);
+    Future <String> Uploadpost(
+        String Title,
+        String Details,
+        dynamic file,
+        String uid,
+        String author,
+        String ppurl
+        )async{
+        String photourl;
+        String err;
+        try{
+            if(file!=null){
+                photourl=await StorageMethods().Storageip("Posts", file, true);
+            }
+            else{
+                photourl="";
+            }
+            String postid= const Uuid().v1();
+            Post post=Post(
+                author_uid: uid,
+                postuid: postid,
+                title: Title,
+                detail: Details,
+                author: author,
+                Timeposted: DateTime.now(),
+                imageUrl: photourl,
+                ppurl: ppurl,
+                likes: [],
+            );
+
+            _firestore.collection("Posts").doc(postid).set(
+                post.toJson(),
+                SetOptions(merge: true),
+            );
+            err="success";
+            return err;
         }
-        else{
-            photourl="";
-        }
-        String postid= const Uuid().v1();
-        Post post=Post(
-            author_uid: uid,
-            postuid: postid,
-            title: Title,
-            detail: Details,
-            author: author,
-            Timeposted: DateTime.now(),
-            imageUrl: photourl,
-            ppurl: ppurl,
-            likes: [],
-        );
-        
-        _firestore.collection("Posts").doc(postid).set(
-            post.toJson(),
-            SetOptions(merge: true),
-        );
-        err="success";
+        catch(e){err=e.toString();
         return err;
+        }
     }
-    catch(e){err=e.toString();
-     return err;
-    }
-}
 
     Future<void>likepost(String postid,String author_uid,List likes)async {
-    try{
-        if(likes.contains(author_uid)){
-           await _firestore.collection("Posts").doc(postid).update(
-                {'likes':FieldValue.arrayRemove([author_uid])}
-            );
+        try{
+            if(likes.contains(author_uid)){
+                await _firestore.collection("Posts").doc(postid).update(
+                    {'likes':FieldValue.arrayRemove([author_uid])}
+                );
             }else{
-          await  _firestore.collection("Posts").doc(postid).update(
-                {'likes':FieldValue.arrayUnion([author_uid])}
-            );
-        }
+                await  _firestore.collection("Posts").doc(postid).update(
+                    {'likes':FieldValue.arrayUnion([author_uid])}
+                );
+            }
 
-    }catch(e){
-    print(e.toString());
+        }catch(e){
+            print(e.toString());
         }
     }
 
 
     Future<String> postcomment(String postid, String text,String author_uid,String author,String ppurl) async{
 
-    try{
-        if(text.isNotEmpty){
-String commentid=const Uuid().v1();
-Comments comments=Comments(author_uid: author_uid, commentuid: commentid, detail: text, author: author, Timeposted: DateTime.now(), );
-_firestore.collection("Posts").doc(postid).collection("comments").doc(commentid).set(comments.toJson());
+        try{
+            if(text.isNotEmpty){
+                String commentid=const Uuid().v1();
+                Comments comments=Comments(
+                    author_uid: author_uid,
+                    commentuid: commentid,
+                    detail: text,
+                    author: author,
+                    Timeposted: DateTime.now(),
+                );
+                _firestore.collection("Posts").doc(postid).collection("comments").doc(commentid).set(
+                    comments.toJson(),
+                    SetOptions(merge: true)
+                );
+            }
+            return "";
+        }catch(e){
+            return e.toString();
         }
-        return "";
-    }catch(e){
-        return e.toString();
-    }
     }
 
 
