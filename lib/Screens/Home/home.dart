6f1e40cd79e717cf9318bb2077d08cc9f8614200
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-02 00:30:14 +0000

    Comments Section

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -219,11 +219,11 @@ class _HomeState extends State<Home> {
                 ListTile(
                   leading: const Icon(LineIcons.alternateSignOut,
                     color: Colors.black,
-                    size:20.0,),
+                    size:30.0,),
                   title: const Text(
                     "Sign Out",
                     style: TextStyle(
-                      fontSize: 12.0,
+                      fontSize: 16.0,
                       fontWeight: FontWeight.bold,
                       fontStyle: FontStyle.italic,
 
