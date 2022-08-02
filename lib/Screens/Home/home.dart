51f49d5d2484d56fb72e986e9b789d434d9bb53a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-02 00:59:30 +0000

    Comments Section

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -186,7 +186,7 @@ class _HomeState extends State<Home> {
 
                           child: Text(
                             user1.Username!,
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 16,
 
                             ),
