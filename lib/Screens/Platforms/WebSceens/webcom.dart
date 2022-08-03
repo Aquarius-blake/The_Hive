33d48ffd6964ee7503269d743e990ab02dd2760e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-03 19:44:33 +0000

    Web Comments Section

--- lib/Screens/Platforms/WebSceens/webcom.dart
+++ lib/Screens/Platforms/WebSceens/webcom.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../../../shared/Widgets/Comment_card.dart';
 
 
 class webcom extends StatefulWidget {
@@ -12,6 +15,26 @@ class webcom extends StatefulWidget {
 class _webcomState extends State<webcom> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("Posts").doc(widget.snap['Post Uid']).collection("comments").orderBy("Comment Time",descending: true).snapshots(),
+          builder: (context,AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshots){
+            if(snapshots.connectionState==ConnectionState.waiting){
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshots.data!.docs.length,
+                itemBuilder: (context, index) => Container(
+                  child: Commentcard(
+                    snap: snapshots.data!.docs[index].data(),
+                    postid: widget.snap['Post Uid'],
+                  ),
+                )
+            );
+          }
+      ),
+    );
   }
 }
