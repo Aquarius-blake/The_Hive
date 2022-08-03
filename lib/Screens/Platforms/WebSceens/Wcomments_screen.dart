1900a9df6273844a1c9004c5373f1c2c35f80914
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-03 19:25:35 +0000

    Web Comments Section

---
+++ lib/Screens/Platforms/WebSceens/Wcomments_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WcommentScreen extends StatefulWidget {
+  const WcommentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WcommentScreen> createState() => _WcommentScreenState();
+}
+
+class _WcommentScreenState extends State<WcommentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
