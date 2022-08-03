42d9fd314065c8f5f333ce7a50681e036107eaff
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-03 21:26:13 +0000

    Edit comment

--- lib/Screens/Platforms/WebSceens/editcomment.dart
+++ lib/Screens/Platforms/WebSceens/editcomment.dart
@@ -1,2 +1,16 @@
 import 'package:flutter/material.dart';
 
+class webcomedit extends StatefulWidget {
+  final snap;
+  const webcomedit({Key? key,this.snap}) : super(key: key);
+
+  @override
+  State<webcomedit> createState() => _webcomeditState();
+}
+
+class _webcomeditState extends State<webcomedit> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
