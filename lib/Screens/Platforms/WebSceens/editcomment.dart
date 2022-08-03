74ada09ab1a42cef8e12f4964838ba738ce3ff0b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-03 21:27:07 +0000

    Edit comment

--- lib/Screens/Platforms/WebSceens/editcomment.dart
+++ lib/Screens/Platforms/WebSceens/editcomment.dart
@@ -13,7 +13,9 @@ class _webcomeditState extends State<webcomedit> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+title: Text(
+    "Edit Comment",
+),
       ),
     );
   }
