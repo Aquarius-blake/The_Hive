b6e0654feb1f3ebea55beeeeeefc724fd7760bf4
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-03 23:18:52 +0000

    Edit comment

--- lib/Screens/Platforms/WebSceens/editcomment.dart
+++ lib/Screens/Platforms/WebSceens/editcomment.dart
@@ -13,6 +13,12 @@ class _webcomeditState extends State<webcomedit> {
   TextEditingController _comment=TextEditingController();
 
   @override
+  void dispose() {
+    _comment.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
