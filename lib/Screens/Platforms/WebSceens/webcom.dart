196e4702c92e0611376ddefdca3ae43e36704f9c
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-03 19:55:34 +0000

    Web Comments Section

--- lib/Screens/Platforms/WebSceens/webcom.dart
+++ lib/Screens/Platforms/WebSceens/webcom.dart
@@ -1,6 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../Models/Users1.dart';
+import '../../../Provider/user_provider.dart';
+import '../../../Services/Firestoremethods.dart';
+import '../../../shared/Pop_up.dart';
 import '../../../shared/Widgets/Comment_card.dart';
 
 
@@ -13,8 +18,33 @@ class webcom extends StatefulWidget {
 }
 
 class _webcomState extends State<webcom> {
+
+  commenting(String postid, String textt,String author_uid,String author,String ppurl) async{
+    String ress=await FirestoreMethods().postcomment(postid, textt, author_uid, author, ppurl);
+    if( ress=="Comment success"){
+      Showsnackbar(ress, context);
+    }else if(ress=="Empty field"){
+      ress="Please Enter text";
+      Showsnackbar(ress, context);
+    }else{
+      Showsnackbar(ress, context);
+    }
+    setState(() {
+      text.text="";
+    });
+
+  }
+  TextEditingController text=TextEditingController();
+
+  @override
+  void dispose() {
+    text.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
+    late  User1 user1=  Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       body: StreamBuilder(
           stream: FirebaseFirestore.instance.collection("Posts").doc(widget.snap['Post Uid']).collection("comments").orderBy("Comment Time",descending: true).snapshots(),
@@ -34,6 +64,57 @@ class _webcomState extends State<webcom> {
                 )
             );
           }
+      ),
+      bottomNavigationBar: SafeArea(
+          child: Container(
+            height: kToolbarHeight,
+            margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom
+            ),
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 8,
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(user1.ppurl!),
+                  radius: 18,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 12.0,
+                        right: 1.0
+                    ),
+                    child: TextField(
+                      controller: text,
+                      decoration: InputDecoration(
+                        hintText: "Comment as ${user1.Username}",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: ()=>commenting(widget.snap['Post Uid'], text.text, user1.UID!, user1.Username!, user1.ppurl!),
+                  child: const Text("Post"),
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      shadowColor: Colors.black,
+                      primary: Colors.lightBlueAccent,
+                      side: const BorderSide(
+                        color: Colors.white70,
+                        width: 2.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100.0)
+                      )
+                  ),
+                )
+              ],
+            ),
+          )
       ),
     );
   }
