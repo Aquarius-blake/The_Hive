acab2ab866d56e376e18a6817cc80de4e0b9ff6a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-04 21:23:23 +0000

    Edit comment

---
+++ lib/Screens/Home/Mobilepages/Meditcomment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mobcomedit extends StatefulWidget {
+  const Mobcomedit({Key? key}) : super(key: key);
+
+  @override
+  State<Mobcomedit> createState() => _MobcomeditState();
+}
+
+class _MobcomeditState extends State<Mobcomedit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
