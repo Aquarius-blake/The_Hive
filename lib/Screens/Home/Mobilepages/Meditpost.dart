2a0bcc77fa86dac2846a15fab18a2a3d09f8d938
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-04 22:16:01 +0000

    Edit post mobile version

---
+++ lib/Screens/Home/Mobilepages/Meditpost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mobeditpost extends StatefulWidget {
+  const Mobeditpost({Key? key}) : super(key: key);
+
+  @override
+  State<Mobeditpost> createState() => _MobeditpostState();
+}
+
+class _MobeditpostState extends State<Mobeditpost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
