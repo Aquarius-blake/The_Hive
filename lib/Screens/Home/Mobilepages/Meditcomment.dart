4f6130a4cad5462fa3593919fd4bd096bcd13ec2
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-04 21:36:28 +0000

    Edit comment

--- lib/Screens/Home/Mobilepages/Meditcomment.dart
+++ lib/Screens/Home/Mobilepages/Meditcomment.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../Models/Users1.dart';
+import '../../../Provider/user_provider.dart';
 
 class Mobcomedit extends StatefulWidget {
   final snap;
@@ -10,8 +14,10 @@ class Mobcomedit extends StatefulWidget {
 }
 
 class _MobcomeditState extends State<Mobcomedit> {
+  TextEditingController text=TextEditingController();
   @override
   Widget build(BuildContext context) {
+    late  User1 user1=  Provider.of<UserProvider>(context).getUser;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -30,6 +36,57 @@ class _MobcomeditState extends State<Mobcomedit> {
         child: Container(
 
         ),
+      ),
+      bottomNavigationBar: SafeArea(
+          child: Container(
+            height: kToolbarHeight,
+            margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom
+            ),
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 8,
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(user1.ppurl!),
+                  radius: 18,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 12.0,
+                        right: 1.0
+                    ),
+                    child: TextField(
+                      controller: text,
+                      decoration: InputDecoration(
+                        hintText: "Comment as ${user1.Username}",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: (){},
+                  child: const Text("Post"),
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      shadowColor: Colors.black,
+                      primary: Colors.lightBlueAccent,
+                      side: const BorderSide(
+                        color: Colors.white70,
+                        width: 2.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100.0)
+                      )
+                  ),
+                )
+              ],
+            ),
+          )
       ),
     );
   }
