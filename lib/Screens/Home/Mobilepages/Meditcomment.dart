390c423e05e66b566c0eec9068c775521ee49211
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-04 21:29:56 +0000

    Edit comment

--- lib/Screens/Home/Mobilepages/Meditcomment.dart
+++ lib/Screens/Home/Mobilepages/Meditcomment.dart
@@ -14,6 +14,7 @@ class _MobcomeditState extends State<Mobcomedit> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
             "Edit Comment",
           style: TextStyle(
