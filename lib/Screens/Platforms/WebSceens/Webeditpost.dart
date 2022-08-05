f43a26abace79718515a897f21b13799ffa60346
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-05 21:22:32 +0000

    Edit Post Web version

---
+++ lib/Screens/Platforms/WebSceens/Webeditpost.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Webeditpost extends StatefulWidget {
+  const Webeditpost({Key? key}) : super(key: key);
+
+  @override
+  State<Webeditpost> createState() => _WebeditpostState();
+}
+
+class _WebeditpostState extends State<Webeditpost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
