5bb2f4fbce027ccd7b559bf307a326f84e559ed2
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-05 21:02:59 +0000

    Edit post mobile version

---
+++ lib/Screens/Platforms/WebSceens/Webed.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Webed extends StatefulWidget {
+  const Webed({Key? key}) : super(key: key);
+
+  @override
+  State<Webed> createState() => _WebedState();
+}
+
+class _WebedState extends State<Webed> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
