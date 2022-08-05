19accd74e15348dd52940c33b42411985197ff2f
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-05 00:20:42 +0000

    Edit post mobile version

--- lib/Screens/Home/Mobilepages/Meditpost.dart
+++ lib/Screens/Home/Mobilepages/Meditpost.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../Services/Upload.dart';
+
 class Mobeditpost extends StatefulWidget {
   final snap;
   const Mobeditpost({Key? key,this.snap}) : super(key: key);
@@ -9,6 +11,11 @@ class Mobeditpost extends StatefulWidget {
 }
 
 class _MobeditpostState extends State<Mobeditpost> {
+  dynamic _image;
+  Upload Selection=Upload();
+  final TextEditingController _title=TextEditingController();
+  final TextEditingController _textEditingController2=TextEditingController();
+  bool _isloading=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
