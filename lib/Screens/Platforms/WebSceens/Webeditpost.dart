5b572d3eafe8bcaa0db2d667c4877319cb031c2d
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-05 23:30:56 +0000

    Edit Post Web version

--- lib/Screens/Platforms/WebSceens/Webeditpost.dart
+++ lib/Screens/Platforms/WebSceens/Webeditpost.dart
@@ -84,6 +84,7 @@ radius: 18,
             SizedBox(
               width: MediaQuery.of(context).size.width*0.7,
               child: TextField(
+                controller: _title,
 decoration: InputDecoration(),
               ),
             ),
