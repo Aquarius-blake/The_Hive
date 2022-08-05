9aa799477c7a08098af686717dfe4ccb6410047e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-05 21:29:12 +0000

    Edit Post Web version

--- lib/Screens/Platforms/WebSceens/Webeditpost.dart
+++ lib/Screens/Platforms/WebSceens/Webeditpost.dart
@@ -10,6 +10,15 @@ class Webeditpost extends StatefulWidget {
 }
 
 class _WebeditpostState extends State<Webeditpost> {
+ dynamic image;
+  Widget Post(dynamic image){
+    return image==null?SizedBox():SizedBox(
+      width: MediaQuery.of(context).size.width*0.3,
+      child: Image.memory(image),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -23,6 +32,7 @@ class _WebeditpostState extends State<Webeditpost> {
               ],
             ),
             SizedBox(),
+            Post(image),
             Divider(),
             Row(
               children: [
