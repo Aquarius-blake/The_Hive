4d8eef9623056bd601176d7872d18b44357ba2c7
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-05 23:27:23 +0000

    Edit Post Web version

--- lib/Screens/Platforms/WebSceens/Webeditpost.dart
+++ lib/Screens/Platforms/WebSceens/Webeditpost.dart
@@ -72,6 +72,7 @@ class _WebeditpostState extends State<Webeditpost> {
 radius: 18,
                   backgroundImage: NetworkImage(user1.ppurl!),
                 ),
+                SizedBox(width: 10,),
                 SizedBox(
                   width: MediaQuery.of(context).size.width*0.5,
                 )
