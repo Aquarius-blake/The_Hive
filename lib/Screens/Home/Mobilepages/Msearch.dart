e7cd84ea2c4535c380007538d06b5ac8a5d5348d
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-07 21:52:30 +0000

    Search function

--- lib/Screens/Home/Mobilepages/Msearch.dart
+++ lib/Screens/Home/Mobilepages/Msearch.dart
@@ -12,8 +12,8 @@ class Msearch extends StatefulWidget {
 class _MsearchState extends State<Msearch> {
   TextEditingController _search=TextEditingController();
   bool isShowuser=false;
-var queryResultset=[];
-var tempSearchstore=[];
+  var queryResultset=[];
+  var tempSearchstore=[];
 
 
   initiateusersearch(value){
@@ -24,22 +24,22 @@ var tempSearchstore=[];
       });
     }
     var capvalue=value.substring(0,1).toUpperCase()+value.substring(1);
-if(queryResultset.length==0 && value.lenth==1 ){
-  SearchService().SearchUser(value).then((QuerySnapshot snapshot){
-    for(int i=0;i<snapshot.docs.length;++i){
-      queryResultset.add(snapshot.docs[i].data());
-    }
-  });
-}else{
-  tempSearchstore=[];
-  queryResultset.forEach((element) {
-    if(element['usernmae'].startsWith(capvalue)){
-      setState(() {
-tempSearchstore.add(element);
+    if(queryResultset.length==0 && value.lenth==1 ){
+      SearchService().SearchUser(value).then((QuerySnapshot snapshot){
+        for(int i=0;i<snapshot.docs.length;++i){
+          queryResultset.add(snapshot.docs[i].data());
+        }
+      });
+    }else{
+      tempSearchstore=[];
+      queryResultset.forEach((element) {
+        if(element['usernmae'].startsWith(capvalue)){
+          setState(() {
+            tempSearchstore.add(element);
+          });
+        }
       });
     }
-  });
-}
   }
 
 
@@ -51,53 +51,53 @@ tempSearchstore.add(element);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-  backgroundColor: Colors.white,
-  elevation: 0.0,
-  title: TextFormField(
-    controller: _search,
-    onFieldSubmitted: (String _){
-      setState(() {
-        isShowuser=true;
-      });
-    },
-    decoration: InputDecoration(
-      labelText: "Search user",
-    ),
-  ),
-),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: TextFormField(
+          controller: _search,
+          onFieldSubmitted: (String _){
+            setState(() {
+              isShowuser=true;
+            });
+          },
+          decoration: InputDecoration(
+            labelText: "Search user",
+          ),
+        ),
+      ),
 
       body: isShowuser? FutureBuilder(
-        future: FirebaseFirestore.instance.collection("users").where("username",isGreaterThanOrEqualTo: _search.text.trim()).get(),
-    builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
-    if(snapshot.connectionState==ConnectionState.waiting){
-    return Center(
-    child: CircularProgressIndicator(),
-    );
-    }else if(!snapshot.hasData){
-      return Center(
-        child: Text("No match found",
-        style: TextStyle(
-            color: Colors.black,
-        ),),
-      );
-    }
-    return ListView.builder(
-      itemCount: snapshot.data!.docs.length,
-        itemBuilder: (context,index){
-        return ListTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(snapshot.data!.docs[index]['profilepic']),
-          ),
-          title: Text(snapshot.data!.docs[index]['username'],
-            style: TextStyle(
-              color: Colors.black
-            ),
-          ),
-        );
-        }
-    );
-    }
+          future: FirebaseFirestore.instance.collection("users").where("username",isGreaterThanOrEqualTo: _search.text.trim()).get(),
+          builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
+            if(snapshot.connectionState==ConnectionState.waiting){
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }else if(!snapshot.hasData){
+              return Center(
+                child: Text("No match found",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context,index){
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(snapshot.data!.docs[index]['profilepic']),
+                    ),
+                    title: Text(snapshot.data!.docs[index]['username'],
+                      style: TextStyle(
+                          color: Colors.black
+                      ),
+                    ),
+                  );
+                }
+            );
+          }
       ):Text("Post"),
     );
   }
