3a2cceafcbe077b314fc13558602c6526721a0b7
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-07 00:23:14 +0000

    Search page

--- lib/Screens/Home/Mobilepages/Msearch.dart
+++ lib/Screens/Home/Mobilepages/Msearch.dart
@@ -37,7 +37,7 @@ appBar: AppBar(
 ),
 
       body: isShowuser? FutureBuilder(
-        future: FirebaseFirestore.instance.collection("users").where("username",isGreaterThanOrEqualTo: _search.text).get(),
+        future: FirebaseFirestore.instance.collection("users").where("username",isGreaterThanOrEqualTo: _search.text.trim()).get(),
     builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
     if(snapshot.connectionState==ConnectionState.waiting){
     return Center(
