345a6b183ba99d79abbd8b27c173c2e45dc7e0b0
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-07 21:35:10 +0000

    Search function

--- lib/Services/Searchmethods.dart
+++ lib/Services/Searchmethods.dart
@@ -1,10 +1,16 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class SearchService{
 
 
     SearchUser(String searchfield){
 
+        return FirebaseFirestore.instance
+            .collection("users")
+            .where("searchkey",isEqualTo: searchfield.substring(0,1).toUpperCase())
+            .get();
     }
 
 
