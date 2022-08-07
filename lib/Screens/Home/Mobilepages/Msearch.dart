b00b38203d3a753b6c065fc02cc8dbe462b656f1
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-07 21:56:54 +0000

    Search function

--- lib/Screens/Home/Mobilepages/Msearch.dart
+++ lib/Screens/Home/Mobilepages/Msearch.dart
@@ -68,7 +68,9 @@ class _MsearchState extends State<Msearch> {
         ),
       ),
 
-      body: isShowuser? FutureBuilder(
+      body: isShowuser? ListView()
+
+      /*FutureBuilder(
           future: FirebaseFirestore.instance.collection("users").where("username",isGreaterThanOrEqualTo: _search.text.trim()).get(),
           builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
             if(snapshot.connectionState==ConnectionState.waiting){
@@ -99,7 +101,7 @@ class _MsearchState extends State<Msearch> {
                 }
             );
           }
-      ):Text("Post"),
+      )*/:Text("Post"),
     );
   }
 }
