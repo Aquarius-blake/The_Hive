87b67995811c5c59561bc18a68dd442db7fa4791
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-08 02:46:07 +0000

    Search function

--- lib/Screens/Platforms/WebSceens/Wsearch.dart
+++ lib/Screens/Platforms/WebSceens/Wsearch.dart
@@ -67,7 +67,7 @@ ElevatedButton(
             width: 2.0,
           ),
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50.0)
+              borderRadius: BorderRadius.circular(200.0)
           )
       ),
     ),
