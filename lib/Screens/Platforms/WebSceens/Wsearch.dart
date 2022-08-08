462850b9c2224f657434ff906a536679a302034b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-08 02:48:38 +0000

    Search function web

--- lib/Screens/Platforms/WebSceens/Wsearch.dart
+++ lib/Screens/Platforms/WebSceens/Wsearch.dart
@@ -13,67 +13,67 @@ class _WsearchState extends State<Wsearch> {
 
   @override
   void dispose() {
-_search.dispose();
+    _search.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-  backgroundColor: Colors.white,
-  title: TextFormField(
-    controller: _search,
-    decoration: const InputDecoration(
-      border: InputBorder.none,
-      label: Text(
-          "Search",
-      style: TextStyle(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextFormField(
+          controller: _search,
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            label: Text(
+              "Search",
+              style: TextStyle(
 
-      ),
-      ),
-    ),
-  ),
-  actions: [
-ElevatedButton(
-    onPressed: (){},
-    child: Text(
-        "User",
-    ),
-  style: ElevatedButton.styleFrom(
-      elevation: 0.0,
-      shadowColor: Colors.black,
-      primary: Colors.blue[400],
-      side: const BorderSide(
-        color: Colors.white70,
-        width: 2.0,
-      ),
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50.0)
-      )
-  ),
-),
-    ElevatedButton(
-      onPressed: (){},
-      child: const Text(
-        "Post",
-      ),
-      style: ElevatedButton.styleFrom(
-          elevation: 0.0,
-          shadowColor: Colors.black,
-          primary: Colors.blue[400],
-          side: const BorderSide(
-            color: Colors.white70,
-            width: 2.0,
+              ),
+            ),
           ),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0)
-          )
-      ),
-    ),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: (){},
+            child: Text(
+              "User",
+            ),
+            style: ElevatedButton.styleFrom(
+                elevation: 0.0,
+                shadowColor: Colors.black,
+                primary: Colors.blue[400],
+                side: const BorderSide(
+                  color: Colors.white70,
+                  width: 2.0,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0)
+                )
+            ),
+          ),
+          ElevatedButton(
+            onPressed: (){},
+            child: const Text(
+              "Post",
+            ),
+            style: ElevatedButton.styleFrom(
+                elevation: 0.0,
+                shadowColor: Colors.black,
+                primary: Colors.blue[400],
+                side: const BorderSide(
+                  color: Colors.white70,
+                  width: 2.0,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)
+                )
+            ),
+          ),
 
-  ],
-),
+        ],
+      ),
     );
   }
 }
