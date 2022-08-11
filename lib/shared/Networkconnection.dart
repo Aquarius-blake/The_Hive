4620a4f5a4f1ac8167f04f8d4a6ac6b748086554
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-11 00:06:42 +0000

    redesign

---
+++ lib/shared/Networkconnection.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Netcon extends StatefulWidget {
+  const Netcon({Key? key}) : super(key: key);
+
+  @override
+  State<Netcon> createState() => _NetconState();
+}
+
+class _NetconState extends State<Netcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
