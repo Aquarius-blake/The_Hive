b2b33a248dea95a33d5158584e75b10645ccd7b9
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-13 08:37:45 +0000

    Search feature web

---
+++ lib/Screens/Home/Mobilepages/Mprofilescreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class Mprofile extends StatefulWidget {
+  const Mprofile({Key? key}) : super(key: key);
+
+  @override
+  State<Mprofile> createState() => _MprofileState();
+}
+
+class _MprofileState extends State<Mprofile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
