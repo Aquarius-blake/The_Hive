db3f8182973238f4f32fe2a6bba9d11b15495e9e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-13 08:39:22 +0000

    Search feature web

---
+++ lib/Screens/Platforms/WebSceens/WprofileScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Wprofile extends StatefulWidget {
+  const Wprofile({Key? key}) : super(key: key);
+
+  @override
+  State<Wprofile> createState() => _WprofileState();
+}
+
+class _WprofileState extends State<Wprofile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
