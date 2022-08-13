ca2b1faf2c2e104004c09ef34acb5a324de1b2a5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-13 07:31:10 +0000

    Search feature web

--- lib/Screens/Platforms/WebSceens/Wsearch.dart
+++ lib/Screens/Platforms/WebSceens/Wsearch.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../../../Services/Searchmethods.dart';
 
 
 
@@ -15,6 +18,85 @@ var queryResultset=[];
 var tempSearchstore=[];
 bool postsearch=false;
 bool loadin=false;
+  initiatepostsearch(value)async{
+    if(value.length==0){
+      setState(() {
+        queryResultset=[];
+        tempSearchstore=[];
+      });
+    }
+    var capvalue=value.substring(0,1).toUpperCase()+value.substring(1);
+    if(queryResultset.length==0 && value.length==1 ){
+      SearchService().SearchPost(value).then((QuerySnapshot<Map<String,dynamic>> snapshot){
+        for(int i=0;i<snapshot.docs.length;++i){
+          queryResultset.add(snapshot.docs[i].data());
+        }
+      });
+    }else{
+      tempSearchstore=[];
+      queryResultset.forEach((element) {
+        if(element['title'].startsWith(capvalue)){
+          setState(() {
+            tempSearchstore.add(element);
+          });
+        }
+      });
+    }
+  }
+
+
+
+  initiateusersearch(value)async{
+    if(value.length==0){
+      setState(() {
+        queryResultset=[];
+        tempSearchstore=[];
+      });
+    }
+    var capvalue=value.substring(0,1).toUpperCase()+value.substring(1);
+    if(queryResultset.length==0 && value.length==1 ){
+      SearchService().SearchUser(value).then((QuerySnapshot<Map<String,dynamic>> snapshot){
+        for(int i=0;i<snapshot.docs.length;++i){
+          queryResultset.add(snapshot.docs[i].data());
+        }
+      });
+    }else{
+      tempSearchstore=[];
+      queryResultset.forEach((element) {
+        if(element['username'].startsWith(capvalue)){
+          setState(() {
+            tempSearchstore.add(element);
+          });
+        }
+      });
+    }
+  }
+
+
+  Widget usercard(data){
+    return GestureDetector(
+      onTap: (){
+
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+        ),
+        elevation: 2.0,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(data['profilepic']),
+              ),
+              Text(data['username'])
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
 
 
