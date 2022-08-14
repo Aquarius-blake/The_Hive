0cd493dd7b6a76a9b4f4a62a477a1678f8a3b9d7
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-14 22:59:35 +0000

    Mobile profile screen

--- lib/Screens/Home/Mobilepages/Mprofilescreen.dart
+++ lib/Screens/Home/Mobilepages/Mprofilescreen.dart
@@ -37,6 +37,7 @@ class _MprofileState extends State<Mprofile> {
                   children: [
 CircleAvatar(
 backgroundImage: NetworkImage(widget.snap['profilepic']),
+  radius: 20,
 )
                   ],
                 )
