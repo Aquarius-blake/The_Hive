e601e463d2e9c84052262e19711a1ac4d00e0a65
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-14 23:25:29 +0000

    Mobile profile screen

--- lib/Screens/Home/Mobilepages/Mprofilescreen.dart
+++ lib/Screens/Home/Mobilepages/Mprofilescreen.dart
@@ -113,10 +113,16 @@ backgroundImage: NetworkImage(widget.snap['profilepic']),
                                   )
                               ),
                                 onPressed: (){},
-                                child: Text(
-                                  "Edit Profile",
-                                  style: TextStyle(
-                                      color: Colors.lightBlueAccent
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left:16.0,
+                                    right: 16
+                                  ),
+                                  child: Text(
+                                    "Edit Profile",
+                                    style: TextStyle(
+                                        color: Colors.lightBlueAccent
+                                    ),
                                   ),
                                 ),
                             )
