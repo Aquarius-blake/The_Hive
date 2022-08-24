38c93a7777aefd3e6bee35a0e8542497db8e03b0
Aquarian Blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-24 10:04:21 +0000

    colo

--- lib/Screens/Home/Mobilepages/Mhome.dart
+++ lib/Screens/Home/Mobilepages/Mhome.dart
@@ -14,6 +14,7 @@ class _MhomeState extends State<Mhome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:Colors. black, 
   body: StreamBuilder(
     stream: FirebaseFirestore.instance.collection('Posts').snapshots(),
       builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
