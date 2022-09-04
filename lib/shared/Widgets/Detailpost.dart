980aced76bf5770b4d851a3715bfd2d8eaeaf6f9
Aquarian Blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-04 17:21:41 +0000

    Create Detailpost.dart

---
+++ lib/shared/Widgets/Detailpost.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:forum3/shared/Widgets/post_card.dart';
+
+
+class detailp extends StatefulWidget {
+  final snap;
+  const detailp({Key? key,this.snap}) : super(key: key);
+
+  @override
+  State<detailp> createState() => _detailpState();
+}
+
+class _detailpState extends State<detailp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Posts').where('Post Uid', isEqualTo: widget.snap['Event Uid']).snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return  PostCard(
+                  snap: snapshot.data!.docs[0].data(),
+
+          );
+        },
+      ),
+    );
+  }
+}
