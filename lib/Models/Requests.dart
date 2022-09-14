ae0701fe43a876d1e4596bf5f0df321b76770763
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-14 20:25:57 +0000

    update

---
+++ lib/Models/Requests.dart
@@ -0,0 +1,51 @@
+
+
+// Custom request Class
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Request{
+
+  late final  String? author_uid;
+  late final String? author;
+  late final  Timeposted;
+  late final String? ppurl;
+  late final String requestuid;
+
+  Request(
+      { required this.author_uid,
+        required this.requestuid,
+        required this.author,
+        required  this.Timeposted,
+        this.ppurl,
+      }
+      );
+
+  Map<String,dynamic> toJson()=>{
+    "author uid":author_uid,
+    "author":author,
+    "Request Time":Timeposted,
+    "Profile Pic":ppurl,
+    "Request Uid":requestuid,
+  };
+
+  static Request? FromSnap(DocumentSnapshot snap){
+    var snapshot= snap.data() as Map<String,dynamic>;
+    Request? Request12=Request(
+      author_uid: snapshot['author uid'],
+      author: snapshot['author'],
+      Timeposted: snapshot['Request Time'],
+      ppurl: snapshot['Profile Pic'],
+      requestuid: snapshot['Request Uid'],
+    );
+
+    return Request12;
+
+
+
+
+
+
+  }
+}
