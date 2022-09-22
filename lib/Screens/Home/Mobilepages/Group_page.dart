bd52192df0ae8d3df8670ada0718b002645c49dc
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-22 22:01:05 +0000

    update Group_page.dart

--- lib/Screens/Home/Mobilepages/Group_page.dart
+++ lib/Screens/Home/Mobilepages/Group_page.dart
@@ -11,7 +11,11 @@ class _GroupPageState extends State<GroupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(),
+      appBar:AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
