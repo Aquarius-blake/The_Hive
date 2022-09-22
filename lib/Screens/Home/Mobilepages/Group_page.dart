bbb7893ecad3df93c1d20ee64c05ec4f073900c7
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-22 21:58:48 +0000

    groupPage

---
+++ lib/Screens/Home/Mobilepages/Group_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class GroupPage extends StatefulWidget {
+  GroupPage({Key? key}) : super(key: key);
+
+  @override
+  State<GroupPage> createState() => _GroupPageState();
+}
+
+class _GroupPageState extends State<GroupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
