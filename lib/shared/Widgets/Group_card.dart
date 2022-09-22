0e0855784e136cd6ec9594a9019dc77d302aceea
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-22 22:09:43 +0000

    group card

---
+++ lib/shared/Widgets/Group_card.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+
+class GroupCard extends StatefulWidget {
+  const GroupCard({ Key? key }) : super(key: key);
+
+  @override
+  State<GroupCard> createState() => _GroupCardState();
+}
+
+class _GroupCardState extends State<GroupCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
