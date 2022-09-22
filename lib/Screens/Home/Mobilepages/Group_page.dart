17e445bfd9538f12114eb1cf94f0d58ddb55f038
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-22 22:34:51 +0000

    update Group_page.dart

--- lib/Screens/Home/Mobilepages/Group_page.dart
+++ lib/Screens/Home/Mobilepages/Group_page.dart
@@ -1,6 +1,11 @@
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:forum3/Models/Users1.dart';
+import 'package:forum3/Provider/user_provider.dart';
+import 'package:forum3/shared/Widgets/Group_card.dart';
+import 'package:provider/provider.dart';
 
 class GroupPage extends StatefulWidget {
   GroupPage({Key? key}) : super(key: key);
@@ -12,6 +17,8 @@ class GroupPage extends StatefulWidget {
 class _GroupPageState extends State<GroupPage> {
   @override
   Widget build(BuildContext context) {
+        late  User1 user1=  Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar:AppBar(
@@ -25,6 +32,31 @@ class _GroupPageState extends State<GroupPage> {
             color:Colors.white,
           ),
           ),
+          actions: [
+            TextButton(
+              onPressed: (){},
+             child: const Text("Create New Group"),
+             )
+          ],
+      ),
+      body:StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Groups').where('Members',arrayContains: user1.UID).snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) => GestureDetector(
+                onTap: (){},
+                child: GroupCard(
+                  snap: snapshot.data!.docs[index].data(),
+                ),
+              )
+          );
+        },
       ),
     );
   }
