11701eb70fc1b36f5d61c8f87e36726f45fc6d77
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-22 22:41:03 +0000

    update Create_groupPage.dart

---
+++ lib/Screens/Home/Mobilepages/Create_groupPage.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+class Creation extends StatefulWidget {
+  const Creation({ Key? key }) : super(key: key);
+
+  @override
+  State<Creation> createState() => _CreationState();
+}
+
+class _CreationState extends State<Creation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
