d27f36485ca7c2d0557a4c5c511394b37f587f7d
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-25 15:54:39 +0000

    group home

---
+++ lib/Screens/Home/Mobilepages/Group_home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Ghome extends StatefulWidget {
+  Ghome({Key? key}) : super(key: key);
+
+  @override
+  State<Ghome> createState() => _GhomeState();
+}
+
+class _GhomeState extends State<Ghome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
