b76f27bba3054026cc91d6d972936310a6ef616a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-25 18:20:21 +0000

    update Group_home.dart

--- lib/Screens/Home/Mobilepages/Group_home.dart
+++ lib/Screens/Home/Mobilepages/Group_home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:forum3/Services/Upload.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 class Ghome extends StatefulWidget {
@@ -12,8 +14,50 @@ class Ghome extends StatefulWidget {
 
 class _GhomeState extends State<Ghome> {
 dynamic image;
+  final Upload Selection=Upload();
 
 
+_selectimage(BuildContext context)async{
+    return showDialog(
+        context: context,
+        builder: (context){
+          return SimpleDialog(
+            title: const Text("Set Group Profile Picture"),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(15.0),
+                child: const Text("Take a Photo"),
+                onPressed: ()async{
+                  Navigator.of(context).pop();
+                  dynamic file=await Selection.uploadpic(ImageSource.camera);
+                  setState(() {
+                    image=file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: EdgeInsets.all(15.0),
+                child: const Text("Choose from gallery"),
+                onPressed: ()async{
+                  Navigator.of(context).pop();
+                  dynamic file=await Selection.uploadpic(ImageSource.gallery);
+                  setState(() {
+                    image=file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(15.0),
+                child: const Text("Cancel"),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +99,9 @@ dynamic image;
                       bottom: 90,
                       left: 150,
                       child: IconButton(
-                        onPressed: (){},
+                        onPressed: ()async{
+                          _selectimage(context);
+                        },
                         icon: const FaIcon(
                           FontAwesomeIcons.camera,
                           size: 50,
