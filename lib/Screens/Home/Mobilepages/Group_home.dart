d7e478b30a109bfc2010e41fd27402bfd1a4cd8e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-25 18:09:11 +0000

    update Group_home.dart

--- lib/Screens/Home/Mobilepages/Group_home.dart
+++ lib/Screens/Home/Mobilepages/Group_home.dart
@@ -40,7 +40,7 @@ class _GhomeState extends State<Ghome> {
                 SizedBox(
                   width: MediaQuery.of(context).size.width*0.9,
                   height: MediaQuery.of(context).size.height*0.3,
-                  child: widget.snap['Header']!="" || widget.snap['Header']!=null? Image.network(
+                  child: widget.snap['Header']!=""? Image.network(
                     "${widget.snap['Header']}",
                     fit: BoxFit.cover,
                     ):Container(
