827a3c90dea212fbe94b022687f4b7cba49896cd
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-25 18:00:16 +0000

    update Group_home.dart

--- lib/Screens/Home/Mobilepages/Group_home.dart
+++ lib/Screens/Home/Mobilepages/Group_home.dart
@@ -34,7 +34,13 @@ class _GhomeState extends State<Ghome> {
       ),
       body:SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            Stack(
+              children: [
+                
+              ],
+            )
+          ],
         ),
       ),
     );
