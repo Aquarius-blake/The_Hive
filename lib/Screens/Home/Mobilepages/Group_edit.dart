7e55a22a2a5cfc6af6709f47a8f67db2dc2cf992
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-27 20:51:16 +0000

    update Group_edit.dart

---
+++ lib/Screens/Home/Mobilepages/Group_edit.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Gedit extends StatefulWidget {
+  const Gedit({ Key? key }) : super(key: key);
+
+  @override
+  State<Gedit> createState() => _GeditState();
+}
+
+class _GeditState extends State<Gedit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
