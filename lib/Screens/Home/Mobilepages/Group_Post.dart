b78c7d475efb0177ff90a17a50f098bec234ef59
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-28 19:31:17 +0000

    update Group_Post.dart

--- lib/Screens/Home/Mobilepages/Group_Post.dart
+++ lib/Screens/Home/Mobilepages/Group_Post.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+
+class GrPost extends StatefulWidget {
+  const GrPost({ Key? key }) : super(key: key);
+
+  @override
+  State<GrPost> createState() => _GrPostState();
+}
+
+class _GrPostState extends State<GrPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
