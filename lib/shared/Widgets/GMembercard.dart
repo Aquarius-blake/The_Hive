253261592131472169ab6f5a023e55f495a23eb8
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-28 20:29:26 +0000

    update GMembercard.dart

--- lib/shared/Widgets/GMembercard.dart
+++ lib/shared/Widgets/GMembercard.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+
+class Gmembercard extends StatefulWidget {
+  final snap;
+  const Gmembercard({ Key? key,this.snap }) : super(key: key);
+
+  @override
+  State<Gmembercard> createState() => _GmembercardState();
+}
+
+class _GmembercardState extends State<Gmembercard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Card()
+    );
+  }
+}
