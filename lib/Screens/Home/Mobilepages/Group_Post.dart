42e347bf24e9d88bd47325e0e7455e9737e86f1a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-29 20:31:17 +0000

    update Group_Post.dart

--- lib/Screens/Home/Mobilepages/Group_Post.dart
+++ lib/Screens/Home/Mobilepages/Group_Post.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:forum3/Models/Users1.dart';
+import 'package:forum3/Services/Upload.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 class GrPost extends StatefulWidget {
@@ -12,6 +14,50 @@ class GrPost extends StatefulWidget {
 
 class _GrPostState extends State<GrPost> {
   dynamic _image;
+  Upload Selection=Upload();
+
+
+ _selectimage(BuildContext context)async{
+    return showDialog(
+        context: context,
+        builder: (context){
+          return SimpleDialog(
+            title: Text("Create Post"),
+            children: [
+              SimpleDialogOption(
+                padding: EdgeInsets.all(15.0),
+                child: Text("Take a Photo"),
+                onPressed: ()async{
+                  Navigator.of(context).pop();
+                  dynamic file=await Selection.uploadpic(ImageSource.camera);
+                  setState(() {
+                    _image=file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: EdgeInsets.all(15.0),
+                child: Text("Choose from gallery"),
+                onPressed: ()async{
+                  Navigator.of(context).pop();
+                  dynamic file=await Selection.uploadpic(ImageSource.gallery);
+                  setState(() {
+                    _image=file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: EdgeInsets.all(15.0),
+                child: Text("Cancel"),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        }
+    );
+  }
 
    Widget Avatar(User1 user1){
     try{
