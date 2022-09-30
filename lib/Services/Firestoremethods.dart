611ecfccaeb3a9f0299a86ca6ae995da33fb0b45
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-30 17:10:28 +0000

    update Firestoremethods.dart

--- lib/Services/Firestoremethods.dart
+++ lib/Services/Firestoremethods.dart
@@ -542,7 +542,7 @@ Future<String> Makerequest(String author,String uid,String ppurl)async{
 
 
 
-  Future GroupPost(String Groupid)async{
+  Future<String> GroupPost(String Groupid)async{
       String ress;
     try{
       ress="Post Successfully";
