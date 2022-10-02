34d2664475fd08735d21325eaf81d067ff7c7518
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-02 18:51:27 +0000

    update Group_edit.dart

--- lib/Screens/Home/Mobilepages/Group_edit.dart
+++ lib/Screens/Home/Mobilepages/Group_edit.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:forum3/Provider/user_provider.dart';
+import 'package:forum3/Services/Firestoremethods.dart';
+import 'package:forum3/Services/Upload.dart';
+import 'package:forum3/shared/Pop_up.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+
+import '../../../Models/Users1.dart';
 
 
 class Gedit extends StatefulWidget {
@@ -10,8 +19,58 @@ class Gedit extends StatefulWidget {
 }
 
 class _GeditState extends State<Gedit> {
+
+dynamic image;
+  final Upload Selection=Upload();
+
+
+_selectimage(BuildContext context)async{
+    return showDialog(
+        context: context,
+        builder: (context){
+          return SimpleDialog(
+            title: const Text("Set Group Profile Picture"),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(15.0),
+                child: const Text("Take a Photo"),
+                onPressed: ()async{
+                  Navigator.of(context).pop();
+                  dynamic file=await Selection.uploadpic(ImageSource.camera);
+                  setState(() {
+                    image=file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: EdgeInsets.all(15.0),
+                child: const Text("Choose from gallery"),
+                onPressed: ()async{
+                  Navigator.of(context).pop();
+                  dynamic file=await Selection.uploadpic(ImageSource.gallery);
+                  setState(() {
+                    image=file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(15.0),
+                child: const Text("Cancel"),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        }
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
+            late  User1 user1=  Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -29,7 +88,55 @@ class _GeditState extends State<Gedit> {
       ),
       body:SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+             Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height*0.3,
+                    child: widget.snap['Header']!=""? Image.network(
+                      "${widget.snap['Header']}",
+                      fit: BoxFit.cover,
+                      ):Container(
+                        color: Colors.grey,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 90,
+                        left: 150,
+                        child: IconButton(
+                          onPressed: ()async{
+                           await _selectimage(context);
+                           if(image!=null){
+                           String content= await FirestoreMethods().UpdateHeader(widget.snap['Group Uid'], image);
+                           Showsnackbar(content, context);
+                           setState(() {
+                             
+                           });
+                           }else{
+                             Showsnackbar("No image selected", context);
+                           }
+                          },
+                          icon:  FaIcon(
+                            FontAwesomeIcons.camera,
+                            size: 50,
+                            color: widget.snap['author uid']==user1.UID && widget.snap['Header']==""? Colors.white:Colors.transparent,
+                            ),
+                          ),
+                      ),
+                      Positioned(
+                        left: 10,
+                        bottom: -50,
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.lightBlueAccent,
+                          backgroundImage: NetworkImage(widget.snap['Group Pic']),
+                          ),
+                        ),
+                ],
+              ),
+          ],
         ),
         )
     );
