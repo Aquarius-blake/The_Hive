72d19902736081d22217e5a11241f020af570d7a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-02 20:08:55 +0000

    update Group_chat.dart

--- lib/Screens/Home/Mobilepages/Group_chat.dart
+++ lib/Screens/Home/Mobilepages/Group_chat.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+
+
+
+class Gchatscreen extends StatefulWidget {
+  final snap;
+  const Gchatscreen({ Key? key,this.snap }) : super(key: key);
+
+  @override
+  State<Gchatscreen> createState() => _GchatscreenState();
+}
+
+class _GchatscreenState extends State<Gchatscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
