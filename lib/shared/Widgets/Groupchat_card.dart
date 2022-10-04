6d794bdf908dba3be28643709a4981d33d514498
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-04 19:46:40 +0000

    update Groupchat_card.dart

--- lib/shared/Widgets/Groupchat_card.dart
+++ lib/shared/Widgets/Groupchat_card.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+
+class Gchatcard extends StatefulWidget {
+  const Gchatcard({ Key? key }) : super(key: key);
+
+  @override
+  State<Gchatcard> createState() => _GchatcardState();
+}
+
+class _GchatcardState extends State<Gchatcard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
