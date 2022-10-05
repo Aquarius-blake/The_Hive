9b603b03513a31da6834a31f3f3474a517bb58fd
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-05 19:23:06 +0000

    update Group_edit.dart

--- lib/Screens/Home/Mobilepages/Group_edit.dart
+++ lib/Screens/Home/Mobilepages/Group_edit.dart
@@ -28,6 +28,15 @@ dynamic image2;
   TextEditingController memalias=TextEditingController();
   TextEditingController postalias=TextEditingController();
 
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.snap["Group Name"]!=null?groupname.text=widget.snap["Group Name"]:"";
+    widget.snap["Group Description"]!=null?groupdesc.text=widget.snap["Group Description"]:"";
+    widget.snap["Identity"]!=null?memalias.text=widget.snap["Identity"]:"";
+    widget.snap["Post Name"]!=null?postalias.text=widget.snap["Post Name"]:"";
+  }
+
 _selectimage2(BuildContext context)async{
     return showDialog(
         context: context,
