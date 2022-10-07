216ba78d6bcefd4ed58df1d783066918d3344082
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-07 19:59:25 +0000

    update Group_chat.dart

--- lib/Screens/Home/Mobilepages/Group_chat.dart
+++ lib/Screens/Home/Mobilepages/Group_chat.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:forum3/Models/Users1.dart';
+import 'package:forum3/Provider/user_provider.dart';
+import 'package:forum3/shared/Pop_up.dart';
+import 'package:provider/provider.dart';
 
 
 
@@ -11,8 +16,14 @@ class Gchatscreen extends StatefulWidget {
 }
 
 class _GchatscreenState extends State<Gchatscreen> {
+
+TextEditingController text=TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
+            late  User1 user1=  Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -40,6 +51,68 @@ class _GchatscreenState extends State<Gchatscreen> {
           iconTheme:const IconThemeData(
             color: Colors.white,
           ),
+      ),
+       bottomNavigationBar: SafeArea(
+          child: Container(
+            height: kToolbarHeight,
+            margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom
+            ),
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 8,
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(user1.ppurl!),
+                  radius: 18,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 12.0,
+                        right: 1.0
+                    ),
+                    child: TextField(
+                      controller: text,
+                      decoration: InputDecoration(
+                        hintText: "Chat as ${user1.Username}",
+                        hintStyle: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: ()async{
+                    String ress="";
+                    Showsnackbar(ress, context);
+                  },
+                  child: const FaIcon(
+                      FontAwesomeIcons.featherPointed
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      shadowColor: Colors.black,
+                      primary: Colors.lightBlueAccent,
+                      side: const BorderSide(
+                        color: Colors.white70,
+                        width: 2.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100.0)
+                      )
+                  ),
+                )
+              ],
+            ),
+          )
       ),
     );
   }
