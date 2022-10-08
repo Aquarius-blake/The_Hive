ee571c29ea720a574d3ae7bb5e8900088a21defd
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-08 16:27:20 +0000

    update Groupchat_card.dart

--- lib/shared/Widgets/Groupchat_card.dart
+++ lib/shared/Widgets/Groupchat_card.dart
@@ -33,6 +33,16 @@ class _GchatcardState extends State<Gchatcard> {
                 const Text("Me")
               ],
             ),
+            const SizedBox(height: 10,),
+            RichText(
+              text: TextSpan(
+                text:widget.snap['message'],
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  )
+                ),
+              ),
           ]
           ),
       ),
