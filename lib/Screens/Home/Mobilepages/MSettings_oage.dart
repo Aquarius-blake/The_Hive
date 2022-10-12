11f1cf55aa65691f1ce3982a655c6d3097b896da
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-12 18:32:25 +0000

    update MSettings_oage.dart

--- lib/Screens/Home/Mobilepages/MSettings_oage.dart
+++ lib/Screens/Home/Mobilepages/MSettings_oage.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class MSettings extends StatefulWidget {
+  const MSettings({ Key? key }) : super(key: key);
+
+  @override
+  State<MSettings> createState() => _MSettingsState();
+}
+
+class _MSettingsState extends State<MSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
