72f7bf24f61804753ae78aadcbe5b095ff8bc438
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-10-29 16:37:12 +0000

    update Settings.dart

--- lib/Models/Settings.dart
+++ lib/Models/Settings.dart
@@ -1,3 +1,6 @@
 //TODO: Create a model to store the settings of the app per each user
 //TODO: Create a settings provider for getting the settings of the app per user
 //TODO: Create settings page for the user to change the settings
+
+
+class Settings{}
