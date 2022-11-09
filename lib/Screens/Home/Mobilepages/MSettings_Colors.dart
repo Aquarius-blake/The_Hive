f83b3e55b50b01f085f116763c4065f2adbfecff
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-11-09 20:57:20 +0000

    update MSettings_Colors.dart

--- lib/Screens/Home/Mobilepages/MSettings_Colors.dart
+++ lib/Screens/Home/Mobilepages/MSettings_Colors.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class CustomThemePage extends StatefulWidget {
+  const CustomThemePage({ Key? key }) : super(key: key);
+
+  @override
+  State<CustomThemePage> createState() => _CustomThemePageState();
+}
+
+class _CustomThemePageState extends State<CustomThemePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
