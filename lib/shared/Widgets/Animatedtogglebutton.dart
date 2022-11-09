13f2f2689bfae82b243a824538978ca3a7e0c2b5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-11-09 19:58:06 +0000

    update Animatedtogglebutton.dart

--- lib/shared/Widgets/Animatedtogglebutton.dart
+++ lib/shared/Widgets/Animatedtogglebutton.dart
@@ -11,7 +11,11 @@ class _AtogglebuttonState extends State<Atogglebutton> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child:AnimatedContainer(
+        duration: Duration(milliseconds: 1000),
+        height:20.0,
+        width: 50.0,
+        )
     );
   }
 }
