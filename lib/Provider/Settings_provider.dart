9e449b351f2ab2939df8dac2326a0e0c2faca297
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-11-09 19:39:20 +0000

    update Settings_provider.dart

--- lib/Provider/Settings_provider.dart
+++ lib/Provider/Settings_provider.dart
@@ -3,7 +3,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:forum3/Models/Settings.dart';
+import 'package:forum3/Services/auth.dart';
 
 class ThemeProvider with ChangeNotifier{
   UserThemeData? _userThemeData;
+  final AuthService _auth=AuthService();
 }
