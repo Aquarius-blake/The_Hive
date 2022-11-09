981419e9e897cd5c53f898c44e27cca8c2225dd9
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-11-09 19:55:41 +0000

    update Animatedtogglebutton.dart

--- lib/shared/Widgets/Animatedtogglebutton.dart
+++ lib/shared/Widgets/Animatedtogglebutton.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Atogglebutton extends StatefulWidget {
+  const Atogglebutton({ Key? key }) : super(key: key);
+
+  @override
+  State<Atogglebutton> createState() => _AtogglebuttonState();
+}
+
+class _AtogglebuttonState extends State<Atogglebutton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
