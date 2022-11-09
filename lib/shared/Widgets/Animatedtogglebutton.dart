13f26f04c10489c758bf8b0b72860f68639c499a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-11-09 20:41:44 +0000

    update Animatedtogglebutton.dart

--- lib/shared/Widgets/Animatedtogglebutton.dart
+++ lib/shared/Widgets/Animatedtogglebutton.dart
@@ -21,32 +21,32 @@ toggleButton(){
   Widget build(BuildContext context) {
     return Container(
       child:AnimatedContainer(
-        duration: Duration(milliseconds: 1000),
-        height:20.0,
-        width: 50.0,
+        duration: Duration(milliseconds: 500),
+        height:40.0,
+        width: 100.0,
         decoration: BoxDecoration(
-          color: toggleValue? Colors.greenAccent[100]: Colors.redAccent[100]!.withOpacity(0.5),
+          color: toggleValue? Colors.lightBlueAccent: Colors.grey.withOpacity(0.5),
           borderRadius: BorderRadius.circular(20.0),
         ),
         child: Stack(
           children: <Widget>[
             AnimatedPositioned(
               curve: Curves.easeIn ,
-              duration: Duration(milliseconds: 1000),
+              duration: Duration(milliseconds: 500),
               top: 3.0,
-              left: toggleValue? 30.0: 0.0,
-              right: toggleValue? 0.0: 30.0,
+              left: toggleValue? 60.0: 0.0,
+              right: toggleValue? 0.0: 60.0,
               child: InkWell(
                 onTap: toggleButton,
                 child: AnimatedSwitcher(
-                  duration: Duration(milliseconds: 1000),
+                  duration: Duration(milliseconds: 500),
                   transitionBuilder: (Widget child, Animation<double> animation){
                     return RotationTransition(
                       turns: animation,
                       child: child,
                     );
                   },
-                  child: toggleValue? Icon(Icons.check_circle, color: Colors.blue, size: 15.0, key: UniqueKey(),): Icon(Icons.remove_circle_outline, color: Colors.white, size: 15.0, key: UniqueKey(),),
+                  child: toggleValue? Icon(Icons.check_circle, color: Colors.blue, size: 30.0, key: UniqueKey(),): Icon(Icons.remove_circle_outline, color: Colors.white, size: 30.0, key: UniqueKey(),),
                 ), 
               ),
               )
