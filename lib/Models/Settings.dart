17c859966f7917896ba3e75c961b7f852ded1641
Aquarius-blake <danbaid72@gmail.com>
2023-01-01 18:24:09 +0000

    update Settings.dart

--- lib/Models/Settings.dart
+++ lib/Models/Settings.dart
@@ -5,7 +5,6 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class UserThemeData{
   late final String? UID;
