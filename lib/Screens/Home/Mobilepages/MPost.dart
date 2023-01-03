03f6deeb1a5bf2414931d91b0b9b63cd35e337b6
Aquarius-blake <danbaid72@gmail.com>
2023-01-03 01:22:30 +0000

    u

--- lib/Screens/Home/Mobilepages/MPost.dart
+++ lib/Screens/Home/Mobilepages/MPost.dart
@@ -83,7 +83,7 @@ class _MpostState extends State<Mpost> {
                 },
               ),
               SimpleDialogOption(
-                padding: EdgeInsets.all(15.0),
+                padding:const EdgeInsets.all(15.0),
                 child: Text("Cancel"),
                 onPressed: (){
                   Navigator.of(context).pop();
