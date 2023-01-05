81653317fa4f2de732a8cdca9b0c4afeb0237d09
Aquarius-blake <danbaid72@gmail.com>
2023-01-05 00:24:20 +0000

    u

--- lib/Screens/Platforms/Layout.dart
+++ lib/Screens/Platforms/Layout.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:forum3/Provider/Settings_provider.dart';
 import 'package:forum3/Screens/Home/home.dart';
 import 'package:forum3/Screens/Platforms/Web.dart';
 import 'package:forum3/shared/dim.dart';
 import 'package:provider/provider.dart';
-
 import '../../Provider/user_provider.dart';
 
 //Seperates the view for android and web based on screen size.
@@ -29,7 +29,10 @@ class _LayoutState extends State<Layout> {
     UserProvider _userprovider=Provider.of(context,listen: false);
     await _userprovider.Refreshuser();
 
-    //TODO: Get Settings provider here and set the theme
+    //TODO: Remove comment to get theme provider
+   /* ThemeProvider _themeProvider=Provider.of(context,listen: false);
+    await _themeProvider.RefreshTheme();*/
+    
 
   }
   @override
