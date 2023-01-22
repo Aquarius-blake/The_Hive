4aafe2f02f8279160e5b95b5f534850858dadffc
Aquarius-blake <danbaid72@gmail.com>
2023-01-22 17:16:44 +0000

    update Comment_card.dart

--- lib/shared/Widgets/Comment_card.dart
+++ lib/shared/Widgets/Comment_card.dart
@@ -32,7 +32,7 @@ class _CommentcardState extends State<Commentcard> {
         horizontal: 16,
       ),
       child: Card(
-        color:Colors.black,
+        color:Color(themedata.CardBackgroundColor),
         child: Container(
           padding: const EdgeInsets.all(5.0),
           child: Column(
