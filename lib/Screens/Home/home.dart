57a1407aa8922b0cf4adb560d19b2bc45f62f1e4
Aquarius-blake <danbaid72@gmail.com>
2023-05-16 11:01:04 +0000

    update home.dart

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -100,7 +100,7 @@ class _HomeState extends State<Home> {
      );
     }
     catch(e){
-    return CircleAvatar(
+    return const CircleAvatar(
       backgroundImage: AssetImage('Assets/hac.jpg'),
       radius: 50,
     );
