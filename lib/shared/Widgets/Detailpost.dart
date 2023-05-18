46b71eb3618bdb72d856c5c67e6a7cfa125a60a9
Aquarius-blake <danbaid72@gmail.com>
2023-05-18 16:09:32 +0000

    update Detailpost.dart

--- lib/shared/Widgets/Detailpost.dart
+++ lib/shared/Widgets/Detailpost.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:forum3/Models/Settings.dart';
+import 'package:forum3/Provider/Settings_provider.dart';
 import 'package:forum3/shared/Widgets/post_card.dart';
+import 'package:provider/provider.dart';
 
 
 class detailp extends StatefulWidget {
@@ -14,7 +17,10 @@ class detailp extends StatefulWidget {
 class _detailpState extends State<detailp> {
   @override
   Widget build(BuildContext context) {
+        late  UserThemeData themedata= Provider.of<ThemeProvider>(context).getUserThemeData;
+
     return Scaffold(
+      backgroundColor: Color(themedata.ScaffoldbackColor),
       body:  StreamBuilder(
         stream: FirebaseFirestore.instance.collection('Posts').where('Post Uid', isEqualTo: widget.snap['Event Uid']).snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
