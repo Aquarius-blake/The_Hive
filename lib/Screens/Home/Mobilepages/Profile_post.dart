c45005c8d7ff991cd2f2b96a52407bfe6dfd311a
Aquarius-blake <danbaid72@gmail.com>
2023-05-23 12:45:14 +0000

    update Profile_post.dart

--- lib/Screens/Home/Mobilepages/Profile_post.dart
+++ lib/Screens/Home/Mobilepages/Profile_post.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:forum3/Models/Settings.dart';
+import 'package:forum3/Provider/Settings_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../../../shared/Widgets/post_card.dart';
 
@@ -15,8 +18,9 @@ class Profilepost extends StatefulWidget {
 class _ProfilepostState extends State<Profilepost> {
   @override
   Widget build(BuildContext context) {
+    late  UserThemeData themedata= Provider.of<ThemeProvider>(context).getUserThemeData;
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Color(themedata.ScaffoldbackColor),
     body:  StreamBuilder(
         stream: FirebaseFirestore.instance.collection('Posts').where('author uid', isEqualTo: widget.snap['uid'] ).snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
