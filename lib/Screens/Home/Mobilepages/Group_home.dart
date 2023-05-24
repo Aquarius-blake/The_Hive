a336e72d71c792c15150a115e0442c52da57b7f4
Aquarius-blake <danbaid72@gmail.com>
2023-05-24 12:20:53 +0000

    update Group_home.dart and Comment_card.dart

--- lib/Screens/Home/Mobilepages/Group_home.dart
+++ lib/Screens/Home/Mobilepages/Group_home.dart
@@ -219,7 +219,7 @@ _selectimage(BuildContext context)async{
                     ),
                     style: ElevatedButton.styleFrom(
                                       elevation: 6.0, 
-                                      backgroundColor: Color(themedata.ScaffoldbuttonColor),
+                                      backgroundColor: Colors.transparent,
                                       shadowColor: Colors.black,
                                       side: const BorderSide(
                                         color: Colors.blue,
