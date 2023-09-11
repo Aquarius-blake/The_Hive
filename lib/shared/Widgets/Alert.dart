ba638af68cbd2889d3275d948906f09977d0608b
Aquarius-blake <danbaid72@gmail.com>
2023-09-11 16:38:15 +0000

    Co-authored-by: derrick <103596268+Derrick-bolton@users.noreply.github.com>

--- lib/shared/Widgets/Alert.dart
+++ lib/shared/Widgets/Alert.dart
@@ -32,3 +32,37 @@ Future<void> showConfirmation(String title,String message,Widget widg,context) a
     },
   );
 }
+
+
+Future<void> showAlert(String title,String message,Widget widg,context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title:  Text(title),
+        content:  SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(message),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text(
+              'Cancel',
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+              ),
+              ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          widg,
+        ],
+      );
+    },
+  );
+}
