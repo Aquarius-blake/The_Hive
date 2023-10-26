bafeb068150ba99986a799ff7bd3923814f12650
Aquarius-blake <danbaid72@gmail.com>
2023-10-26 16:16:28 +0000

    test: update user_repository_test.dart

--- test/model/user_repository_test.dart
+++ test/model/user_repository_test.dart
@@ -6,3 +6,7 @@ import 'package:mockito/mockito.dart';
 class MockFirebaseAuth extends Mock implements FirebaseAuth{}
 class MockFirebaseUser extends Mock implements User{}
 class MockAuthResult extends Mock implements UserCredential{}
+
+void main(){
+  
+}
