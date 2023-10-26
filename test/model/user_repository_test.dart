1ad85f1eeadbaf4008318c1ea45276f16ea4d8c2
Aquarius-blake <danbaid72@gmail.com>
2023-10-26 16:19:44 +0000

    test: update user_repository_test.dart

--- test/model/user_repository_test.dart
+++ test/model/user_repository_test.dart
@@ -2,11 +2,13 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mockito/mockito.dart';
+import 'package:rxdart/subjects.dart';
 
 class MockFirebaseAuth extends Mock implements FirebaseAuth{}
 class MockFirebaseUser extends Mock implements User{}
 class MockAuthResult extends Mock implements UserCredential{}
 
 void main(){
-  
+  MockFirebaseAuth _auth = MockFirebaseAuth();
+  BehaviorSubject<MockFirebaseUser> _user = BehaviorSubject<MockFirebaseUser>();
 }
