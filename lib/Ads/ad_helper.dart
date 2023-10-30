48dd3a90f35d494014783697ab1a773ae8dcc464
Aquarius-blake <danbaid72@gmail.com>
2023-10-30 14:51:27 +0000

    ads test

---
+++ lib/Ads/ad_helper.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+class AdHelper {
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/6300978111';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544/2934735716';
+    } else {
+      throw new UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/1033173712";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/4411468910";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/5224354917";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/1712485313";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+}
