38caed312d51d9e64d90f784b70cfa6e76c0b4ab
Aquarius-blake <danbaid72@gmail.com>
2023-12-02 22:46:22 +0000

    update Group_explore.dart

--- lib/Screens/Home/Mobilepages/Group_explore.dart
+++ lib/Screens/Home/Mobilepages/Group_explore.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:forum3/Ads/ad_helper.dart';
 import 'package:forum3/Models/Settings.dart';
 import 'package:forum3/Provider/Settings_provider.dart';
 import 'package:forum3/Screens/Home/Mobilepages/Group_home.dart';
 import 'package:forum3/shared/Widgets/Group_card.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
 class GroupExplore extends StatefulWidget {
@@ -13,6 +15,45 @@ class GroupExplore extends StatefulWidget {
 }
 
 class _GroupExploreState extends State<GroupExplore> {
+  BannerAd? _bannerAd;
+
+  Widget ad(index){
+   if ((_bannerAd != null) && (index % 8 == 0)){
+    return Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: _bannerAd!.size.width.toDouble(),
+                height: _bannerAd!.size.height.toDouble(),
+                child: AdWidget(ad: _bannerAd!),
+              ),
+            );
+   } else {
+    return Container();
+   }   
+}
+
+@override
+  void initState() {
+    BannerAd(
+    adUnitId: AdHelper.bannerAdUnitId,
+    request: AdRequest(),
+    size: AdSize.banner,
+    listener: BannerAdListener(
+      onAdLoaded: (ad) {
+        setState(() {
+          _bannerAd = ad as BannerAd;
+        });
+      },
+      onAdFailedToLoad: (ad, err) {
+        print('Failed to load a banner ad: ${err.message}');
+        ad.dispose();
+      },
+    ),
+  ).load();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     late  UserThemeData themedata= Provider.of<ThemeProvider>(context).getUserThemeData;
