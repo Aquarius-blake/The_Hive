918b30d085ff5f535390756639991920f42551c9
Aquarius-blake <danbaid72@gmail.com>
2023-12-21 11:58:10 +0000

    dzone

---
+++ lib/Screens/Home/Mobilepages/MDanger_zone.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MDangerzone extends StatefulWidget {
+
+  @override
+  State<MDangerzone> createState() => _MDangerzoneState();
+}
+
+class _MDangerzoneState extends State<MDangerzone> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
